703b1e7beabc2448dfa2b2994cff0c943117c8eb
AmadeusEnki <milanenmars@gmail.com>
2022-12-22 16:05:46 +0100

    SetupPageAbout

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,42 @@
 <template>
-  <div>
-    <h2>
-      This is an about page
-      <h4>Example heading <span class="badge bg-secondary">New</span></h4>
-    </h2>
-  </div>
-  <button type="button" class="btn btn-primary position-relative">
-    Inbox
-    <span
-      class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-    >
-      99+
-      <span class="visually-hidden">unread messages</span>
-    </span>
-  </button>
+  <div class="container-fluid" :style="image">
+    <div id="app" class="container py-2 px-0">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://www.liquor.com/thmb/zzY3QbT5h9S9utBkexhECycXZIc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-519728153-7dca4b18c59f4b1fa3654e4d5c9db884.jpg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Les bières artisanals sont les meilleurs bieres du coin</h5>
+              <p class="card-text">Les bieres viennent d'une contrée lointaine très lointaine au dela des montagnes et des rivières
+              </p>
+            </div>
+          </div>
+        </div>
+          <div class="col-md-4">
+            <div class="card card-body">Hello</div>
+          </div>
+          <div class="col-md-5">
+            <div class="card card-body"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: {
+        backgroundImage:
+          "url(https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=all)",
+      },
+    };
+  },
+};
+</script>
