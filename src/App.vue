2b7257ba61176ffef121bb938fb4ccab32eb788c
fredy <fredy.rodriguez@he-arc.ch>
2022-12-23 01:03:18 +0100

    Modifications page Home

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ th,
 td {
   text-align: center;
   vertical-align: middle;
-},
+}
+,
 .material-icons-sharp {
   font-variation-settings: "FILL" 0, "wght" 200, "GRAD" 0, "opsz" 480;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,237 @@
 <template>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Bienvenue </title>
-  </head>
-  <body>
-      <h1>Bienvenue</h1>
-      <h2>1. Ce que nous vous offrons</h2>
-      <p>Une application de gestion de cave à bière</p>
-     <p>Pratique, simple et rapide !</p>
-      <h2>2. Pourquoi nous choisir ?</h2>
-      <div>
-    <p>Pourquoi nous choisir :</p>
-    <P>Il y a plusieurs raisons pour lesquelles on pourrait vouloir utiliser une application de gestion de bière :
-      <ul class="list-group">
-  <li class="list-group-item">Pour suivre sa consommation de bière : une application de gestion de bière peut aider à suivre combien de bières ont été consommées et à quelle fréquence. Cela peut être utile pour ceux qui essaient de limiter leur consommation de bière ou qui veulent simplement être conscients de leur consommation.
-</li>
-  <li class="list-group-item">Pour enregistrer les différentes bières testées : une application de gestion de bière peut aider à enregistrer les différentes bières testées, y compris le type de bière, la brasserie et les notes de dégustation. Cela peut être utile pour ceux qui aiment essayer de nouvelles bières et qui veulent garder une trace de leurs préférées.
-</li>
-  <li class="list-group-item">Pour suivre les bières en stock : une application de gestion de bière peut aider à suivre les bières en stock et à savoir quand il est temps de s'approvisionner en bière. Cela peut être utile pour ceux qui ont une grande collection de bières et qui veulent s'assurer de ne jamais être à court de leur bière préférée.
-</li>
-  <li class="list-group-item">Pour partager ses goûts en bière avec d'autres : une application de gestion de bière peut permettre de partager ses goûts en bière avec d'autres utilisateurs de l'application. Cela peut être utile pour trouver de nouvelles bières à essayer ou pour recevoir des recommandations de bières de la part d'autres amateurs de bière.
-</li>
-</ul>
-</P>
-</div>
-<div classe="formulaireQuestion">
-  <h2>Une question ou sugestion ?</h2>
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-</div>
-  </body>
-</html>
-  
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <title>Bienvenue</title>
+      <link
+        href="https://getbootstrap.com/docs/5.2/examples/carousel/carousel.css"
+        rel="stylesheet"
+      />
+    </head>
+    <body>
+      <main>
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide-to="0"
+              class="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <svg
+                class="bd-placeholder-img"
+                width="100%"
+                height="100%"
+                aria-hidden="true"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <rect width="100%" height="100%" fill="#74D0F1" />
+              </svg>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <img
+                    id="img"
+                    src="https://media.revistagq.com/photos/5f0d9c5b42f91a64e56906c7/16:9/w_1920,c_limit/george-cox-l9Z93oauxgs-unsplash.jpg"
+                  />
+                  <p class="text text-center">
+                    Refresh-toi avec la meilleur biere du moment
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <svg
+                class="bd-placeholder-img"
+                width="100%"
+                height="100%"
+                aria-hidden="true"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <rect width="100%" height="100%" fill="#74D0F1" />
+              </svg>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <img
+                    id="img"
+                    src="https://www.letribunaldunet.fr/wp-content/uploads/2020/10/bieres.jpg"
+                  />
+                  <p>Le vrai partage de la vie</p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <svg
+                class="bd-placeholder-img"
+                width="100%"
+                height="100%"
+                aria-hidden="true"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <rect width="100%" height="100%" fill="#74D0F1" />
+              </svg>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <img
+                    id="img"
+                    src="https://storage.lebonguide.com/crop-1600x700/81/10/F3CDF8D1-365F-4AB4-9C33-1F2E8A08C4A0.png"
+                  />
+                  <p>Produits de notre région</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+
+          <div class="container marketing"></div>
+        </div>
+
+        <!-- Trois columnes de texte au-dessous de carousel -->
+        <div class="row text-center">
+          <div class="col-lg-4">
+            <img class="img-cercle"
+              src="https://www.lamentounasc.fr/file/si916015/biere-entre-amis-fi17423796x640.jpg"/>
+
+            <h2>Partage</h2>
+            <p>
+              Les producteurs de la region collecte le meilleur blés pour vous
+            </p>
+            <p>
+              <a class="btn btn-secondary" href="#">View details &raquo;</a>
+            </p>
+          </div>
+          <div class="col-lg-4">
+              <img class="img-cercle"
+              src="https://c8.alamy.com/compfr/pwahe0/letiquette-des-aliments-biologiques-sains-bio-produit-de-haute-qualite-et-de-badges-100-eco-bio-et-naturels-sur-licone-du-produit-emblemes-pour-cafe-lemballage-etc-vector-pwahe0.jpg">
+            <h2>100% Bio</h2>
+            <p>
+              Another exciting bit of representative placeholder content. This
+              time, we've moved on to the second column.
+            </p>
+            <p>
+              <a class="btn btn-secondary" href="#">View details &raquo;</a>
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <img class="img-cercle"
+            src="https://thumbs.dreamstime.com/z/made-switzerland-badge-made-switzerland-round-seal-ribbon-switzerland-made-switzerland-121703186.jpg">
+            <h2>Produit de qualité</h2>
+            <p>
+              And lastly this, the third column of representative placeholder
+              content.
+            </p>
+            <p>
+              <a class="btn btn-secondary" href="#">View details &raquo;</a>
+            </p>
+          </div>
+          <hr class="featurette-divider" />
+
+          <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading">
+                Les bières artisanals sont les meilleurs bieres du coin.
+                <span class="text-muted">Vraiment les meilleures</span>
+              </h2>
+              <p class="lead">
+                Les bieres viennent d'une contrée lointaine très lointaine au dela des montagnes et des rivières
+              </p>
+            </div>
+            <div class="col-md-5">
+              <img id="img1"
+                src="https://www.liquor.com/thmb/zzY3QbT5h9S9utBkexhECycXZIc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-519728153-7dca4b18c59f4b1fa3654e4d5c9db884.jpg">
+            </div>
+          </div>
+          <hr class="featurette-divider" />
+
+          <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+              <h2 class="featurette-heading">
+                Oh yeah, it’s that good.
+                <span class="text-muted">See for yourself.</span>
+              </h2>
+              <p class="lead">
+                Another featurette? Of course. More placeholder content here to
+                give you an idea of how this layout would work with some actual
+                real-world content in place.
+              </p>
+            </div>
+            <div class="col-md-5 order-md-1">
+              <img
+                class=".img-fluid"
+                id="img1"
+                src="https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=all"
+              />
+            </div>
+          </div>
+          <hr class="featurette-divider" />
+        </div>
+      </main>
+      <!-- FOOTER -->
+      <footer class="container" color="#74D0F1">
+        
+        <p class="float-end"><a href="#">Back to top</a></p>
+        <p>
+          &copy; 2017–2021 Company, Inc. &middot;
+          <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
+        </p>
+        
+      </footer>
+    </body>
+  </html>
 </template>
-<style>
 
-body{
-  padding: 10px 50px 20px;
+<style scoped>
+#img {
+  max-width: 5000px;
+  max-height: 390px;
 }
-
+#img1 {
+  max-width: 500px;
+  max-height: 500px;
+}
+.img-cercle {
+    width: 140px;
+    height: 140px;
+    border: 180px;
+    border-radius: 180px;
+}
 </style>
