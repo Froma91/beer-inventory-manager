c342c3f3f90870aa5b88838d69633ad0717d3dc1
fredy <fredyrodriguez91@hotmail.com>
2023-01-09 13:12:49 +0100

    Modification liste des bieres

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,12 +1,18 @@
 <template>
-    <hr class="featurette-divider" />
-    <footer class="text-muted py-5">
+  <hr class="featurette-divider" />
+  <footer class="text-muted py-5">
     <div class="container">
-    <p class="float-end mb-1">
+      <p class="float-end mb-1">
         <a href="#">Back to top</a>
-    </p>
-    <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/">getting started guide</a>.</p>
+      </p>
+      <p class="mb-1">
+        Album example is &copy; Bootstrap, but please download and customize it
+        for yourself!
+      </p>
+      <p class="mb-0">
+        New to Bootstrap? <a href="/">Visit the homepage</a> or read our
+        <a href="/">getting started guide</a>.
+      </p>
     </div>
-    </footer>
+  </footer>
 </template>
